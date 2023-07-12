04aebecbd127a205080f759bb9efcf5b1210f164
Victoria Kruszewski <vickrusz@gmail.com>
2023-07-11 23:53:21 -0500

    Icons, favorites, and Comments

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Card, Icon } from "react-native-elements";
 
-const RenderCampsite = ({ campsite }) => {
+const RenderCampsite = ( props ) => {
+    const { campsite } = props;
     if (campsite) {
         return (
             <Card containerStyle={styles.cardContainer}>
@@ -20,12 +21,16 @@ const RenderCampsite = ({ campsite }) => {
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
                 <Icon
-                    name='heart-o'
-                    type='font-awsome'
+                    name={props.isFavorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
                     color='#f50'
                     raised
                     reverse
-                    
+                    onPress={() => 
+                        props.isFavorite 
+                        ? console.log("Already set as a favorite") 
+                        : props.markFavorite()
+                    }
                 />
             </Card>
         );
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -7,6 +7,7 @@ const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
 
     const [ comments, setComments] = useState(COMMENTS);
+    const [ favorite, setFavorite] = useState(false);
 
     const renderCommentItem = ({ item }) => {
         return (
@@ -31,7 +32,11 @@ const CampsiteInfoScreen = ({ route }) => {
             }}
             ListHeaderComponent={
                 <>
-                    <RenderCampsite campsite={campsite} />
+                    <RenderCampsite 
+                        campsite={campsite}
+                        isFavorite={favorite}
+                        markFavorite={() => setFavorite(true)}
+                    />
                     <Text style={styles.commentsTitle}>Comments</Text>
                 </>
             }
