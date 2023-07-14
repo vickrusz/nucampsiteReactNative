44ffd568451075f675acb5a1d57cf8da162543fd
Victoria Kruszewski <vickrusz@gmail.com>
2023-07-14 08:19:53 -0500

    Using Redux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,7 @@
 import { Text, ScrollView } from 'react-native'
 import { Avatar, Card, ListItem } from 'react-native-elements';
-import { useState } from 'react';
-import { PARTNERS } from '../shared/partners';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
 const Mission = () => {
     return (
@@ -26,7 +26,7 @@ const Mission = () => {
 };
 
 const AboutScreen = () => {
-    const [partners, setPartners] = useState(PARTNERS);
+    const partners = useSelector((state) => state.partners);
 
     return (
         <ScrollView>
@@ -34,9 +34,9 @@ const AboutScreen = () => {
             <Card>
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
-                {partners.map((partner) => (
+                {partners.partnersArray.map((partner) => (
                     <ListItem key={partner.id}>
-                        <Avatar rounded source={partner.image}></Avatar>
+                        <Avatar rounded source={{ uri: baseUrl + partner.image }} />
                         <ListItem.Content>
                             <ListItem.Title>{partner.name}</ListItem.Title>
                             <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
