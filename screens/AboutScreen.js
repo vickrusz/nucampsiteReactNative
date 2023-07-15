f1e59ec21997495081dc5ad3b8f7229ccad3beb2
Victoria Kruszewski <vickrusz@gmail.com>
2023-07-14 20:35:30 -0500

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { Text, ScrollView } from 'react-native'
 import { Avatar, Card, ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
 const Mission = () => {
     return (
@@ -28,6 +29,33 @@ const Mission = () => {
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+            <Mission />
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
+                <Loading />
+            </Card>
+        </ScrollView>
+        );
+    }
+
+    if (partners.errMess) {
+
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+    
     return (
         <ScrollView>
             <Mission />
