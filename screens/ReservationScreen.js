fad5776c3e22d01b8dddbb4647cd3e8532a7984d
Victoria Kruszewski <vickrusz@gmail.com>
2023-07-24 15:01:30 -0500

    workshop 3

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import {
     Text,
-    View, 
-    Modal, 
+    View,  
     ScrollView, 
     StyleSheet,
     Switch, 
-    Button
+    Button,
+    Alert
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
@@ -18,8 +18,6 @@ const ReservationScreen = () => {
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
 
-    const [showModal, setShowModal] = useState(false);
-
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
         setShowCalendar(Platform.OS === 'ios');
@@ -30,7 +28,33 @@ const ReservationScreen = () => {
         console.log('campers:', campers);
         console.log('hikeIn:', hikeIn);
         console.log('date:', date);
-        setShowModal(!showModal);
+
+        Alert.alert(`Begin Search? \n
+            Number of campers: ${campers}\n
+            hike In? ${hikeIn}\n
+            Date ${date.toLocaleDateString('en-US')}\n`
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => {
+                        console.log(
+                            'Cancel pressed'
+                        ),
+                    resetForm();
+                    }, style:'cancel'
+                },
+                {
+                    text: 'OK',
+                    onPress: () => {
+                        console.log(
+                            'Ok pressed'
+                        ),
+                    resetForm();
+                    }, 
+                        
+                }
+            ], { cancelable: false }
+        );
     };
 
     const resetForm = () => {
@@ -96,37 +120,7 @@ const ReservationScreen = () => {
                             color='#5637DD'
                             accessibilityLabel="Tap me to search for available campsites to reserve"
                         />
-
                     </View>
-                    <Modal
-                        animationType='slide'
-                        transparent={false}
-                        visible={showModal}
-                        onRequestClose={() => setShowModal(!showModal)}
-                    >
-                        <View style={styles.modal}>
-                            <Text style={styles.modalTitle}>
-                                Search Campsite Reservations
-                            </Text>
-                            <Text style={styles.modalText}>
-                                Number of Campers: {campers}
-                            </Text>
-                            <Text style={styles.modalText}>
-                                Hike-In?: {hikeIn ? 'Yes' : 'No'}
-                            </Text>
-                            <Text style={styles.modalText}>
-                                Date: {date.toLocaleDateString('en-US')}
-                            </Text>
-                            <Button
-                                onPress={() => {
-                                    setShowModal(!showModal);
-                                    resetForm();
-                                }}
-                                color='#5637DD'
-                                title='Close'
-                            />
-                        </View>
-                    </Modal>
             </Animatable.View>
         </ScrollView>
     );
@@ -147,22 +141,6 @@ const styles = StyleSheet.create({
     formItem: {
         flex: 1
     },
-    modal: {
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
-    }
 });
 
 export default ReservationScreen;
